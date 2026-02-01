277bc289bb2c61ccf3236aa5a68d57f3729a1faf
ralphreevencarandang <rvncrndng@gmail.com>
2026-02-01 12:05:24 +0800

    add stagger animation in certificate page

--- app/certificates/page.tsx
+++ app/certificates/page.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+'use client'
 import Link from 'next/link'
 import { ArrowLeft } from 'lucide-react'
 import { certificates } from '@/constants'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
 const page = () => {
+
+
+    useGSAP(()=> {
+
+        gsap.from('#certificate', {
+            opacity: 0,
+            stagger: 0.2,
+            duration: 0.5,
+            ease: "power4.inOut"
+
+        })
+
+    }, [])
   return (
     <div className=' max-w-5xl padding-x py-10 space-y-5'>
 
@@ -17,10 +32,10 @@ const page = () => {
 
             </div>
 
-            <div className=' grid grid-cols-1 sm:grid-cols-2 gap-3'>
+            <div className=' grid grid-cols-1 sm:grid-cols-2 gap-3' >
 
                 {certificates.map( (item, index)=> 
-                    <Link key={index} className='card space-y-2' href={item.link} target='_blank'>
+                    <Link key={index} className='card space-y-2' href={item.link} target='_blank' id='certificate'>
                             <h3 className='font-bold'>{item.title}</h3>
                             <p className='text-sm text-zinc-500 '>{item.provider}</p>
                    
